1a5c01d2e744217de14e58c66695bcf6d4943fc6
jasdev <js2wt@virginia.edu>
2013-09-06 00:03:22 -0400

    refreshing ignore and more scaffolds

--- heatmap-gen/heatmap-gen.py
+++ heatmap-gen/heatmap-gen.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from pprint import pprint
 import soundcloud
 
@@ -7,20 +8,29 @@ client = soundcloud.Client(client_id="6504a6ad70b88dd5684e256947c782c0")
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    page_size = 200
+def getCommentsFromURL(target):
+    track = client.get('/resolve', url=target)
+    id = track.id
 
+    page_size = 200
     comments = []
-    current = client.get('/tracks/108569036/comments', limit=page_size)
+    current = client.get('/tracks/' + str(id) + '/comments', limit=page_size)
     comments += current
     page = 1
     while not len(current) == 0:
-        current = client.get('/tracks/108569036/comments', limit=page_size, offset=page*page_size)
+        current = client.get('/tracks/' + str(id) + '/comments', limit=page_size, offset=page*page_size)
         comments += current
         page += 1
-    print len(comments)
-    return 'Hello World!'
+    return len(comments)
+
+@app.route('/')
+def index():
+    return 'lol index page'
+
+@app.route('/comments')
+def getComments():
+    url = request.args.get('url')
+    return str(getCommentsFromURL(target=url))
 
 
 if __name__ == '__main__':
