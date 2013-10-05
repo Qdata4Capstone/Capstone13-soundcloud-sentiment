3f0b17e20dff1a94cb5e391b6ac64b16605b1ebf
jasdev <js2wt@virginia.edu>
2013-10-05 18:19:02 -0400

    checkpoint

--- heatmap-gen/static/js/index.js
+++ heatmap-gen/static/js/index.js
@@ -1,4 +1,12 @@
-var waveform = new Waveform({
-  container: document.getElementById("test"),
-  data: [1, 0.2, 0.5]
+function drawWaveForm(){
+    var waveform = new Waveform({
+        container: $('#test')[0],
+        data: [1, 0.2, 0.5]
+    });
+}
+
+$(document).ready(function(){
+    $('#trigger').on('click', function(){
+        drawWaveForm();
+    });
 });
