e434746c2218e55596239161015456f20004413c
jasdev <js2wt@virginia.edu>
2013-10-06 13:12:20 -0400

    comment scoring in place, trying to hook up on js side

--- heatmap-gen/heatmap_gen.py
+++ heatmap-gen/heatmap_gen.py
@@ -11,77 +11,77 @@ app.config.from_envvar('SC_SENTIMENT_SETTINGS')
 client = soundcloud.Client(client_id=app.config['SOUNDCLOUD_ID'])
 os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 
-##Pull out all of the words in a list of tagged tweets, formatted in tuples.
-#def getwords(tweets):
-#    allwords = []
-#    for (words, sentiment) in tweets:
-#        allwords.extend(words)
-#    return allwords
-#
-##Order a list of tweets by their frequency.
-#def getwordfeatures(listoftweets):
-#    #Print out wordfreq if you want to have a look at the individual counts of words.
-#    wordfreq = nltk.FreqDist(listoftweets)
-#    words = wordfreq.keys()
-#    return words
-#
-#def feature_extractor(doc):
-#    docwords = set(doc)
-#    features = {}
-#    for i in wordlist:
-#        features['contains(%s)' % i] = (i in docwords)
-#    return features
-#
-#rp = open('./comments/really_positive.txt', 'r')
-#really_positive = rp.readlines()
-#
-#sp = open('./comments/semi_positive.txt', 'r')
-#semi_positive = sp.readlines()
-#
-#n = open('./comments/neutral.txt', 'r')
-#neutral = n.readlines()
-#
-#neg = open('./comments/negative.txt', 'r')
-#negative = neg.readlines()
-#
-#neglist, rplist, splist, neutlist = [], [], [], []
-#
-#for i in range(0,len(really_positive)):
-#    rplist.append('really_positive')
-#
-#for i in range(0,len(semi_positive)):
-#    splist.append('semi_positive')
-#
-#for i in range(0,len(neutral)):
-#    neutlist.append('neutral')
-#
-#for i in range(0,len(negative)):
-#    neglist.append('negative')
-#
-#rptagged = zip(really_positive, rplist)
-#sptagged = zip(semi_positive, splist)
-#neuttagged = zip(neutral, neutlist)
-#negtagged = zip(negative, neglist)
-#
-#taggedtweets = negtagged + neuttagged + sptagged + rptagged
-#
-#tweets = []
-#
-#for (word, sentiment) in taggedtweets:
-#    word_filter = [i.lower() for i in word.split()]
-#    tweets.append((word_filter, sentiment))
-#
-#customstopwords = ['download', 'link', 'music', 'synth', 'i\'m', 'u', 'balls', 'dl',
-#                   'people', 'chords', 'you\'re', 'new', 'im', 'guys', 'i\'ve', 'vocals',
-#                   'would', 'one', 'like', 'tell', 'joel!', 'martin', 'r', 'hardwell', '?',
-#                   'that\'s', 'check', '...']
-#
-#wordlist = wordlist = [i for i in getwords(tweets) if not i in stopwords.words('english')]
-#wordlist = [i for i in wordlist if not i in customstopwords]
-#
-#training_set = nltk.classify.apply_features(feature_extractor, tweets)
-#classifier = nltk.NaiveBayesClassifier.train(training_set)
-#
+#Pull out all of the words in a list of tagged tweets, formatted in tuples.
+def getwords(tweets):
+    allwords = []
+    for (words, sentiment) in tweets:
+        allwords.extend(words)
+    return allwords
+
+#Order a list of tweets by their frequency.
+def getwordfeatures(listoftweets):
+    #Print out wordfreq if you want to have a look at the individual counts of words.
+    wordfreq = nltk.FreqDist(listoftweets)
+    words = wordfreq.keys()
+    return words
+
+def feature_extractor(doc):
+    docwords = set(doc)
+    features = {}
+    for i in wordlist:
+        features['contains(%s)' % i] = (i in docwords)
+    return features
+
+rp = open('./comments/really_positive.txt', 'r')
+really_positive = rp.readlines()
+
+sp = open('./comments/semi_positive.txt', 'r')
+semi_positive = sp.readlines()
+
+n = open('./comments/neutral.txt', 'r')
+neutral = n.readlines()
+
+neg = open('./comments/negative.txt', 'r')
+negative = neg.readlines()
+
+neglist, rplist, splist, neutlist = [], [], [], []
+
+for i in range(0,len(really_positive)):
+    rplist.append('really_positive')
+
+for i in range(0,len(semi_positive)):
+    splist.append('semi_positive')
+
+for i in range(0,len(neutral)):
+    neutlist.append('neutral')
+
+for i in range(0,len(negative)):
+    neglist.append('negative')
+
+rptagged = zip(really_positive, rplist)
+sptagged = zip(semi_positive, splist)
+neuttagged = zip(neutral, neutlist)
+negtagged = zip(negative, neglist)
+
+taggedtweets = negtagged + neuttagged + sptagged + rptagged
+
+tweets = []
+
+for (word, sentiment) in taggedtweets:
+    word_filter = [i.lower() for i in word.split()]
+    tweets.append((word_filter, sentiment))
+
+customstopwords = ['download', 'link', 'music', 'synth', 'i\'m', 'u', 'balls', 'dl',
+                   'people', 'chords', 'you\'re', 'new', 'im', 'guys', 'i\'ve', 'vocals',
+                   'would', 'one', 'like', 'tell', 'joel!', 'martin', 'r', 'hardwell', '?',
+                   'that\'s', 'check', '...']
+
+wordlist = wordlist = [i for i in getwords(tweets) if not i in stopwords.words('english')]
+wordlist = [i for i in wordlist if not i in customstopwords]
+
+training_set = nltk.classify.apply_features(feature_extractor, tweets)
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+
 #print classifier.show_most_informative_features(n=50)
 
 def get_comments_from_url(target):
@@ -117,22 +117,25 @@ def get_comments_from_url(target):
 
         page += 1
 
-    result = {'id': id, 'waveform_url': waveform, 'duration': duration,'comments': comments}
+    scores = score_comments(comments, duration)
+
+    result = {'id': id, 'waveform_url': waveform, 'duration': duration,'comments': comments, 'scores': scores}
     return result
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/waveform')
-def waveform():
-    url = request.args.get('url')
-    '''
-    Below is snippet to classify a given piece of text
-        print "Attempt to classify: " + classifier.classify(feature_extractor("good tune".split()))
-    Need to fetch all comments form URL
-    Classify each one, and pass to template for rendering with waveform.js
-    '''
+def score_comments(comments, time):
+    scores = {}
+    for comment in comments:
+        try:
+            percent = float(str(comment['timestamp'])) / time
+            scores['' + "{0:.8f}".format(percent)] = classifier.classify(feature_extractor(comment['body'].split()))
+        except ValueError:
+            pass
+    return scores
+
 
 @app.route('/comments')
 def get_comments():
--- heatmap-gen/static/js/index.js
+++ heatmap-gen/static/js/index.js
@@ -1,7 +1,26 @@
 function drawWaveForm(){
     var waveform = new Waveform({
         container: $('#test')[0],
-        data: [1, 0.2, 0.5]
+        data: [0.3, 0.5, 1],
+        innerColor: function(x, y){
+            if (scores[x.toFixed(6)] !== undefined) {
+                if(scores[x.toFixed(6)] == "really_positive"){
+                    console.log("rp hit");
+                    return '#00FF00';
+                }
+                else if (scores[x.toFixed(6)] == "semi_positive"){
+                    console.log("sp hit");
+                    return '#FFFF00';
+                }
+                else{
+                    console.log("else hit");
+                    return '#FFFFFF';
+                }
+            }else {
+                console.log("no comment hit");
+                return '#FFFFFF';
+            }
+        }
     });
 }
 
