95bcccca62da06cc64a48a1bd7dad66edbc46df5
jasdev <js2wt@virginia.edu>
2013-11-10 17:14:47 -0500

    csv version of top 100

--- heatmap-gen/top_100.py
+++ heatmap-gen/top_100.py
@@ -1,17 +1,27 @@
 import json
+import csv
 
 if __name__ == '__main__':
     json_data = open('../datasets/dj-mag-top-100.json')
     data = json.load(json_data)
-    uniques = set()
-    for x in range(1997, 2014):
-        for name in data[str(x)]:
-            uniques.add(name)
+    table = []
+    table.append(["Rank"])
+    table[0].extend(range(1997, 2014))
 
-    uniques = sorted(list(uniques))
+    count = 1
+    for rank in range(0, 100):
+        table.append([rank + 1])
+        for x in range(1997, 2014):
+            table[count].append(data[str(x)][rank])
+        count = count + 1
 
-    for name in uniques:
-        print(name)
+    fl = open('dj-mag-top-100.csv', 'w')
+
+    writer = csv.writer(fl)
+    for values in table:
+        writer.writerow(values)
+
+    fl.close()
 
     #Close file stream
     json_data.close()
