7802a3f3eba614bb2a787828f486e7801552ea14
jasdev <js2wt@virginia.edu>
2013-11-17 00:53:11 -0500

    starting graph

--- heatmap-gen/top_100.py
+++ heatmap-gen/top_100.py
@@ -2,26 +2,17 @@ import json
 import csv
 
 if __name__ == '__main__':
-    json_data = open('../datasets/dj-mag-top-100.json')
+    json_data = open('static/datasets/dj-mag-top-100.json')
     data = json.load(json_data)
-    table = []
-    table.append(["Rank"])
-    table[0].extend(range(1997, 2014))
+    uniques = set()
 
-    count = 1
-    for rank in range(0, 100):
-        table.append([rank + 1])
-        for x in range(1997, 2014):
-            table[count].append(data[str(x)][rank])
-        count = count + 1
+    for year in range(1997, 2014):
+        for name in data[str(year)]:
+            uniques.add(name)
 
-    fl = open('../datasets/dj-mag-top-100.csv', 'w')
 
-    writer = csv.writer(fl)
-    for values in table:
-        writer.writerow(values)
-
-    fl.close()
+    print "[" + ",".join(uniques) + "]"
+    print len(uniques)
 
     #Close file stream
     json_data.close()
